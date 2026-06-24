205df46ada0768d8bf3cca254298b30f55c4b7e5
PriyaPandey19 <pp479021@gmail.com>
2026-06-24 11:30:43 +0530

    working

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,7 +4,7 @@
 import axios from 'axios';
 
 //base url sab routes yahan se shuru hote hain
-const api = axios.add({
+const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || 'https://khataai-project.onrender.com/api'
 });
 
